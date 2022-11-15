d0e34ef185ea10d875c80044aeb22678663e7578
eyalItzhak <62293316+eyalItzhak@users.noreply.github.com>
2022-11-15 14:48:41 +0200

    mobx base

---
+++ Reactivities/clinet-app/src/app/stores/activityStore.ts
@@ -0,0 +1,14 @@
+import { makeObservable,observable } from "mobx";
+
+
+export default class ActivityStore{
+    title = "hello from mobx";
+
+
+    constructor () {
+        makeObservable(this,{
+            title: observable
+        })
+    }
+
+}
